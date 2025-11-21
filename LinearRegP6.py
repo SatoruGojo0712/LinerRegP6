c3ae06b0c6f135b9b9b619692bc8448262e31ec3
SatoruGojo0712 <ananyasharma13112008@gmail.com>
2025-11-21 15:00:50 +0530

    Add files via upload

---
+++ LinearRegP6.py
@@ -0,0 +1,27 @@
+import pandas as pd    
+import numpy as np  
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv("C:\Users\Ravikant\Pictures\AI practicals\2020neetrankscore.csv")
+print (df.head(10))
+
+x = df[['Marks']]
+y = df['Rank']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state = 42)
+
+model=LinearRegression()
+model.fit(x_train,y_train)
+
+st.title("Rank Predictor")
+st.write("Enter your score to predict your Rank")
+
+Score = st.number_input("Score calculated:", min_value=0.0, step=1.0)
+
+if st.button("Predict Rank"):
+     predicted_rank = model.predict([[Score]])[0]
+     st.success(f"Predicted Rank: {predicted_rank:2f}")
+
+st.write("Sample Data")
+st.dataframe(df)     
